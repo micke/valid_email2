343018b4b27f6cc3fd1598675eccf9c414ec7de7
Micke Lisinge <lisinge@gmail.com>
2013-07-19 22:22:12 +0200

    Version 1.0.0

--- lib/valid_email2.rb
+++ lib/valid_email2.rb
@@ -1,7 +1,7 @@
 require "valid_email2/email_validator"
 
 module ValidEmail2
-  VERSION = "0.0.4"
+  VERSION = "1.0.0"
 
   def self.disposable_emails
     @@disposable_emails ||= YAML.load_file(File.expand_path("../../vendor/disposable_emails.yml",__FILE__))
