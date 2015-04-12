3966ad1f30f74080ca1c110114b2e8deb12d4b1a
Alexandr Prudnikov <al.prudnikov@gmail.com>
2015-04-12 20:50:07 +0300

    Option for load deleted domains

--- lib/valid_email2.rb
+++ lib/valid_email2.rb
@@ -1,12 +1,26 @@
-require "valid_email2/email_validator"
+require 'valid_email2/email_validator'
 
 module ValidEmail2
+  def self.load_deleted=(value)
+    @@load_deleted = value
+    @@disposable_emails = nil
+  end
+
   def self.disposable_emails
-    @@disposable_emails ||= YAML.load_file(File.expand_path("../../vendor/disposable_emails.yml",__FILE__))
+    @@disposable_emails ||= load_disposable_emails
   end
 
   def self.blacklist
-    blacklist_file = "vendor/blacklist.yml"
+    blacklist_file = 'vendor/blacklist.yml'
     @@blacklist ||= File.exists?(blacklist_file) ? YAML.load_file(File.expand_path(blacklist_file)) : []
+  end
+
+  protected
+
+  def self.load_disposable_emails
+    dir = File.expand_path('../../vendor', __FILE__)
+    standard = YAML.load_file(File.join(dir, 'disposable_emails.yml'))
+    deleted = @@load_deleted ? YAML.load_file(File.join(dir, 'disposable_emails_deleted.yml')) : []
+    standard + deleted
   end
 end
