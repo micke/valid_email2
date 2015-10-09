3646bb247b3f4038a78d3f419016140ef9425609
Micke Lisinge <lisinge@gmail.com>
2015-10-09 09:51:40 +0200

    Add a whitelist to pull_mailchecker_emails.rb
    So that we can whitelist domains such as poczta.onet.pl which is in fact
    a legit email provider

--- pull_mailchecker_emails.rb
+++ pull_mailchecker_emails.rb
@@ -1,20 +1,19 @@
 #!/usr/bin/env ruby
 
-require 'rubygems'
-require 'pry'
+require "yaml"
 
-require 'yaml'
+require "json"
+require "net/http"
 
-require 'json'
-require 'net/http'
+whitelisted_emails = %w(poczta.onet.pl)
 
-existing_emails = YAML.load_file('vendor/disposable_emails.yml')
+existing_emails = YAML.load_file("vendor/disposable_emails.yml")
 
-url = 'https://raw.githubusercontent.com/FGRibreau/mailchecker/master/list.json'
+url = "https://raw.githubusercontent.com/FGRibreau/mailchecker/master/list.json"
 resp = Net::HTTP.get_response(URI.parse(url))
 
-remote_emails = JSON.parse(resp.body).flatten
+remote_emails = JSON.parse(resp.body).flatten - whitelisted_emails
 
 result_emails = (existing_emails + remote_emails).map(&:strip).uniq.sort
 
-File.open('vendor/new_disposable_emails.yml', 'w') {|f| f.write result_emails.to_yaml }
+File.open("vendor/disposable_emails.yml", "w") {|f| f.write result_emails.to_yaml }
