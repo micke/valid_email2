f4d870fa5c32e2d76367169abe9b0778948ded36
Micke Lisinge <me@mike.gg>
2016-12-17 21:36:55 +0100

    Upgrade rspec to 3.5.0

--- spec/valid_email2_spec.rb
+++ spec/valid_email2_spec.rb
@@ -21,69 +21,69 @@ describe ValidEmail2 do
     subject(:user) { TestUser.new(email: "") }
 
     it "should be valid when email is empty" do
-      user.valid?.should be_true
+      expect(user.valid?).to be_truthy
     end
 
     it "should not be valid when domain is missing" do
       user = TestUser.new(email: "foo")
-      user.valid?.should be_false
+      expect(user.valid?).to be_falsey
     end
 
     it "should be invalid when email is malformed" do
       user = TestUser.new(email: "foo@bar")
-      user.valid?.should be_false
+      expect(user.valid?).to be_falsey
     end
 
     it "should be invalid if Mail::AddressListsParser raises exception" do
       user = TestUser.new(email: "foo@gmail.com")
-      Mail::Address.stub(:new).and_raise(Mail::Field::ParseError.new(nil, nil, nil))
-      user.valid?.should be_false
+      expect(Mail::Address).to receive(:new).and_raise(Mail::Field::ParseError.new(nil, nil, nil))
+      expect(user.valid?).to be_falsey
     end
 
     it "shouldn't be valid if the domain constains consecutives dots" do
       user = TestUser.new(email: "foo@bar..com")
-      user.valid?.should be_false
+      expect(user.valid?).to be_falsey
     end
   end
 
   describe "disposable emails" do
     it "should be valid when the domain is not in the list of disposable email providers" do
       user = TestUserDisallowDisposable.new(email: "foo@gmail.com")
-      user.valid?.should be_true
+      expect(user.valid?).to be_truthy
     end
 
     it "should be invalid when domain is in the list of disposable email providers" do
       user = TestUserDisallowDisposable.new(email: "foo@#{ValidEmail2.disposable_emails.first}")
-      user.valid?.should be_false
+      expect(user.valid?).to be_falsey
     end
 
     it "should be invalid when domain is a subdomain of a disposable domain" do
       user = TestUserDisallowDisposable.new(email: "foo@bar.#{ValidEmail2.disposable_emails.first}")
-      user.valid?.should be_false
+      expect(user.valid?).to be_falsey
     end
   end
 
   describe "blacklisted emails" do
     it "should be valid when email is not in the blacklist" do
       user = TestUserDisallowBlacklisted.new(email: "foo@gmail.com")
-      user.valid?.should be_true
+      expect(user.valid?).to be_truthy
     end
 
     it "should be invalid when email is in the blacklist" do
       user = TestUserDisallowBlacklisted.new(email: "foo@#{ValidEmail2.blacklist.first}")
-      user.valid?.should be_false
+      expect(user.valid?).to be_falsey
     end
   end
 
   describe "mx lookup" do
     it "should be valid if mx records are found" do
       user = TestUserMX.new(email: "foo@gmail.com")
-      user.valid?.should be_true
+      expect(user.valid?).to be_truthy
     end
 
     it "should be invalid if no mx records are found" do
       user = TestUserMX.new(email: "foo@subdomain.gmail.com")
-      user.valid?.should be_false
+      expect(user.valid?).to be_falsey
     end
   end
 end
--- valid_email2.gemspec
+++ valid_email2.gemspec
@@ -22,7 +22,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake", "~> 11.3.0"
-  spec.add_development_dependency "rspec", "~> 2.14.1"
+  spec.add_development_dependency "rspec", "~> 3.5.0"
   spec.add_runtime_dependency "mail", "~> 2.5"
   spec.add_runtime_dependency "activemodel", ">= 3.2"
 end
