micke/valid_email2

commit c92c566e759113ad1c43a8c7824b9d38be3138ea

Change disposable_email_domains to be txt file

lib/valid_email2.rb
@@ -3,18 +3,17 @@
 require "valid_email2/email_validator"
 
 module ValidEmail2
-  BLACKLIST_FILE = "config/blacklisted_email_domains.yml"
-  WHITELIST_FILE = "config/whitelisted_email_domains.yml"
+  BLACKLIST_FILE  = "config/blacklisted_email_domains"
+  WHITELIST_FILE  = "config/whitelisted_email_domains.yml"
+  DISPOSABLE_FILE = File.expand_path('../config/disposable_email_domains.txt', __dir__)
 
   def self.disposable_emails
-    @disposable_emails ||= load_file(
-      File.expand_path('../config/disposable_email_domains.yml', __dir__)
-    )
+    @disposable_emails ||= File.open(DISPOSABLE_FILE){ |f| f.read }.split("\n")
   end
 
   def self.blacklist
-    @blacklist ||= if File.exist?(BLACKLIST_FILE)
-                     load_file(File.expand_path(BLACKLIST_FILE))
+    @whitelist ||= if File.exist?(BLACKLIST_FILE)
+                     YAML.load_file(File.expand_path(BLACKLIST_FILE))
                    else
                      []
                    end
@@ -22,13 +21,9 @@ module ValidEmail2
 
   def self.whitelist
     @whitelist ||= if File.exist?(WHITELIST_FILE)
-                     load_file(File.expand_path(WHITELIST_FILE))
+                     YAML.load_file(File.expand_path(WHITELIST_FILE))
                    else
                      []
                    end
-  end
-
-  def self.load_file(path)
-    Set.new(YAML.load_file(path))
   end
 end

commit 21b51161cd94452ea8c233aa1d35d777e95bbcbf

Fix lookup performance (again)
Regression by c92c566e759113ad1c43a8c7824b9d38be3138ea

closes #137

lib/valid_email2.rb
@@ -8,22 +8,35 @@ module ValidEmail2
   DISPOSABLE_FILE = File.expand_path('../config/disposable_email_domains.txt', __dir__)
 
   def self.disposable_emails
-    @disposable_emails ||= File.open(DISPOSABLE_FILE){ |f| f.read }.split("\n")
+    @disposable_emails ||= load_file(DISPOSABLE_FILE)
   end
 
   def self.blacklist
-    @blacklist ||= if File.exist?(BLACKLIST_FILE)
-                     YAML.load_file(File.expand_path(BLACKLIST_FILE))
-                   else
-                     []
-                   end
+    @blacklist ||= load_if_exists(BLACKLIST_FILE)
   end
 
   def self.whitelist
-    @whitelist ||= if File.exist?(WHITELIST_FILE)
-                     YAML.load_file(File.expand_path(WHITELIST_FILE))
-                   else
-                     []
-                   end
+    @whitelist ||= load_if_exists(WHITELIST_FILE)
+  end
+
+private
+
+  def self.load_if_exists(path)
+    File.exist?(path) ? load_file(path) : Set.new
+  end
+
+  def self.load_file(path)
+    # This method MUST return a Set, otherwise the
+    # performance will be as bad as hell!
+    if path.end_with?(".yml")
+      Set.new(YAML.load_file(path))
+    else
+      result = Set.new
+      File.open(path, "r").each_line do |line|
+        line.strip!
+        result << line if line.present?
+      end
+      result
+    end
   end
 end