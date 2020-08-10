07a302cfa2cd059b16275e278c107976e4696795
Micke Lisinge <hi@micke.me>
2020-08-10 14:09:59 +0200

    Use class << self

--- lib/valid_email2.rb
+++ lib/valid_email2.rb
@@ -7,36 +7,38 @@ module ValidEmail2
   WHITELIST_FILE  = "config/whitelisted_email_domains.yml"
   DISPOSABLE_FILE = File.expand_path('../config/disposable_email_domains.txt', __dir__)
 
-  def self.disposable_emails
-    @disposable_emails ||= load_file(DISPOSABLE_FILE)
-  end
+  class << self
+    def disposable_emails
+      @disposable_emails ||= load_file(DISPOSABLE_FILE)
+    end
 
-  def self.blacklist
-    @blacklist ||= load_if_exists(BLACKLIST_FILE)
-  end
+    def blacklist
+      @blacklist ||= load_if_exists(BLACKLIST_FILE)
+    end
 
-  def self.whitelist
-    @whitelist ||= load_if_exists(WHITELIST_FILE)
-  end
+    def whitelist
+      @whitelist ||= load_if_exists(WHITELIST_FILE)
+    end
 
-private
+    private
 
-  def self.load_if_exists(path)
-    File.exist?(path) ? load_file(path) : Set.new
-  end
+    def load_if_exists(path)
+      File.exist?(path) ? load_file(path) : Set.new
+    end
 
-  def self.load_file(path)
-    # This method MUST return a Set, otherwise the
-    # performance will suffer!
-    if path.end_with?(".yml")
-      Set.new(YAML.load_file(path))
-    else
-      result = Set.new
-      File.open(path, "r").each_line do |line|
-        line.strip!
-        result << line if line.present?
+    def load_file(path)
+      # This method MUST return a Set, otherwise the
+      # performance will suffer!
+      if path.end_with?(".yml")
+        Set.new(YAML.load_file(path))
+      else
+        result = Set.new
+        File.open(path, "r").each_line do |line|
+          line.strip!
+          result << line if line.present?
+        end
+        result
       end
-      result
     end
   end
 end
