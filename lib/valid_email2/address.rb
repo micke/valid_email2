34dd381027101a96c754b2aaa9ed7badf96806e1
cm-dyoshikawa <yoshikawa.daio@classmethod.jp>
2025-02-05 10:48:25 +0900

    fix: Adjust domain hierarchy depth check to start from 3

--- lib/valid_email2/address.rb
+++ lib/valid_email2/address.rb
@@ -124,7 +124,7 @@ module ValidEmail2
       return false if tokens.length < 3
 
       max_depth = [@domain_hierarchy_max_depth, tokens.length].min
-      (2..max_depth).each do |depth|
+      (3..max_depth).each do |depth|
         partial_domain = tokens.reverse.first(depth).reverse.join('.')
         return true if domain_list.include?(partial_domain)
       end
